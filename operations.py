bc37a8097c9df10e02b7f05cb4dc0a78407e03e8
Reima <reima.luoma@gmail.com>
2023-03-06 10:47:34 +0200

    moved option handling from weekly to operations and call it from there in weekly

--- operations.py
+++ operations.py
@@ -11,3 +11,12 @@ def printRootMenu():
     print('*---------------------------------*')
 
     print('Enter your choice [number] >>')
+
+def handleRootMenuChoice(option):
+    match option:
+        case '1':
+            print('adding person')
+        case '2':
+            print('adding task')
+        case _:
+            print('not really an option, is it?')
--- weekly.py
+++ weekly.py
@@ -4,10 +4,4 @@ operations.printRootMenu()
 
 option = input()
 
-match option:
-    case '1':
-        print('adding person')
-    case '2':
-        print('adding task')
-    case _:
-        print('not really an option, is it?')
+operations.handleRootMenuChoice(option)
