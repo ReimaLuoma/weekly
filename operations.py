74d01162a7d41841882b630a8f90198a963d9de5
Reima <reima.luoma@gmail.com>
2023-03-06 11:46:43 +0200

    added createPerson function to operations
    rootmenu uses createPerson function now for appropriate option

--- operations.py
+++ operations.py
@@ -1,3 +1,5 @@
+import person
+
 def printRootMenu():
     '''
     Prints root menu
@@ -13,10 +15,25 @@ def printRootMenu():
     print('Enter your choice [number] >>')
 
 def handleRootMenuChoice(option):
+    '''
+    Handles choice made in root menu
+    :param option: this is the choice made by user
+    '''
     match option:
         case '1':
-            print('adding person')
+            createPerson()
         case '2':
             print('adding task')
         case _:
             print('not really an option, is it?')
+
+def createPerson():
+    print('Give name of the person >>')
+    name = input()
+    print('Give age of {name}')
+    age = input()
+
+    newPerson = person.Person(name, age)
+    
+    print('_______________________')
+    print('Added: ', newPerson)
