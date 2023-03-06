380f592bd956cce4fab2bc269dbab604040a03f7
Reima <reima.luoma@gmail.com>
2023-03-06 10:45:04 +0200

    added new file operations to handle repeating actions as functions
    weekly now uses operations for printing Root Menu

---
+++ operations.py
@@ -0,0 +1,13 @@
+def printRootMenu():
+    '''
+    Prints root menu
+    '''
+    print('*---------------------------------*')
+
+    print('Select option:')
+    print('[1] Add person')
+    print('[2] Add task')
+
+    print('*---------------------------------*')
+
+    print('Enter your choice [number] >>')
--- weekly.py
+++ weekly.py
@@ -1,12 +1,6 @@
-print('*---------------------------------*')
+import operations
 
-print('Select option:')
-print('[1] Add person')
-print('[2] Add task')
-
-print('*---------------------------------*')
-
-print('Enter your choice [number] >>')
+operations.printRootMenu()
 
 option = input()
 
